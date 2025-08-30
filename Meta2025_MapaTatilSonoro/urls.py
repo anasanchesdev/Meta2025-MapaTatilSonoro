94c62685d863a8c73423497d97204cb141c7edaa
Dev. Sanches <dev.anaclarasanches@gmail.com>
2025-08-30 10:18:40 -0300

    Arquivos de configuração do projeto (Meta2025_MapaTatilSonoro) / Arquivos de configuração do app (mapa_tatil_sonoro)

--- Meta2025_MapaTatilSonoro/urls.py
+++ Meta2025_MapaTatilSonoro/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MapaTatilSonoro project.
+URL configuration for Meta2025_MapaTatilSonoro project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- Meta2025_MapaTatilSonoro/wsgi.py
+++ Meta2025_MapaTatilSonoro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MapaTatilSonoro project.
+WSGI config for Meta2025_MapaTatilSonoro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MapaTatilSonoro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Meta2025_MapaTatilSonoro.settings')
 
 application = get_wsgi_application()
